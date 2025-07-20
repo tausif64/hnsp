6acd7e6288a3b509c9b66606a0f97376874f17c4
tausif64 <developer.642003@gmail.com>
2025-07-20 10:22:24 +0530

    hooks

---
+++ src/hooks/useAppDispatch.ts
@@ -0,0 +1,6 @@
+// hooks/useAppDispatch.ts
+import { useDispatch } from "react-redux";
+import type { AppDispatch } from "../store/store";
+
+
+export const useAppDispatch = () => useDispatch<AppDispatch>();
